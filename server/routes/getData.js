5f16fcb2189f85af04248b741590e746f68277f9
hellobrother1472 <paras1472@gmail.com>
2022-09-06 15:31:35 +0530

    auth, productModel,added middleware

---
+++ server/routes/getData.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const User = require('../database/models/User');
+const router = express.Router();
+
+const app = express();
+
+app.get("/productData",(req,res)=>{
+
+})
+
+
+// add pictures in product model
+// product description
+// get productData 
+// get categoryData 
+
+// admin panel route and authentication(/admin/login)
+// for postData admin middleware  
+// add data and remove product and category
+
+
+// email and mobile notification on checkout
+// Forgot password feature
+// reset password routes
+// order history feature (add data after buy)
+// addToCart (data saved in local storage)
+
+module.exports = router;
