f1c44697cf04989772bae46cbf3731f5c378d299
Rishabh Jain <rishuj.rj1995@gmail.com>
2022-09-07 19:52:01 +0530

    Made some changes in getData file

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -14,6 +14,10 @@ const userSchema = mongoose.Schema({
         type:String,
         required:true,
     },
+    cpassword:{
+        type:String,
+        required:true,
+    },
     number:{
         type: Number,
         required:true,
