e0e27dff8dcb3173dc879bbcbe4ecd38aae4136a
hellobrother1472 <paras1472@gmail.com>
2022-09-07 14:39:37 +0530

    admin's authentication and middleware added

---
+++ server/middlewares/adminAuthentication.js
@@ -0,0 +1,24 @@
+var express = require('express');
+const jwt = require('jsonwebtoken');
+const Admin = require("../database/models/Admin")
+
+const adminAuthentication = async (req,res,next)=>{
+    try {
+        const token = req.cookies.jwt;
+        const decode = jwt.verify(token,process.env.JWT_ADMIN_SECRET);
+        
+        const admin = await Admin.findById(decode.admin.id);
+        if(!admin){
+            res.status(401).send({ error: "Admin authentication error" })
+            throw new Error("Admin authentication error");
+        }
+        req.admin = admin;
+        next();
+        
+    } catch (error) {
+        console.log(error);
+        res.status(404).send({error:error.message});
+    }
+}
+
+module.exports = adminAuthentication;
