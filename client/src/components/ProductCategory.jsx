97978514ebc452d31bc865b709fdd4e6b74ae6aa
hellobrother1472 <paras1472@gmail.com>
2022-10-15 12:31:55 +0530

    small changes

--- client/src/components/ProductCategory.jsx
+++ client/src/components/ProductCategory.jsx
@@ -11,7 +11,6 @@ const ProductCategory = () => {
         const fetchData = async () => {
             const response = await fetch(`http://localhost:5000/admin/getAllProductsByCategory/${id}`);
             const data = await response.json();
-            console.log(data);
             setProducts(data.productList);
         }
 
@@ -28,7 +27,7 @@ const ProductCategory = () => {
                 {
                     products && products.map((product) => (
                         <Link to = {`/product/${product._id}`}>
-                        <div className='product bg-white flex flex-col justify-center items-center p-2 mx-6 my-4 cursor-pointer relative hover:scale-110 hover:shadow-2xl hover:border-2 border-red-500 duration-200' key={product._id}>
+                        <div className='product bg-white flex flex-col justify-center items-center p-2 mx-6 my-4 cursor-pointer relative hover:scale-110 hover:shadow-2xl duration-200' key={product._id}>
                             <h4 className='flex justify-center items-center text-white bg-red-500 px-1 text-sm rounded-md gap-1 absolute top-2 right-2'>4.5 <AiFillStar /></h4>
                             <div className='image bg-gray-200'>
                                 <img className='h-72 w-64' src={`http://localhost:5000/${product.productImage}`} alt='shirt' />
@@ -36,7 +35,7 @@ const ProductCategory = () => {
                             <div className='product-detail flex flex-col justify-center items-center mt-3'>
                                 <h3 className='text-xl text-red-500'>{product.name}</h3>
                                 <div className='price flex gap-2 items-center justify-center'>
-                                    <h4>{product.price}</h4>
+                                    <h4>₹{product.price}/-</h4>
                                     <h4 className='text-gray-500 line-through text-sm'>₹1299/-</h4>
                                 </div>
                                 <h4 className='text-gray-500 text-center break-words'>{product.description}</h4>
