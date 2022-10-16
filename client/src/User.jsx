ba8a5787854c3ae94bffe08b7aeccf2227809e93
hellobrother1472 <paras1472@gmail.com>
2022-10-16 14:20:44 +0530

    login logout bug fix

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Navbar from "./components/Navbar";
 import FooterTwo from "./components/FooterTwo";
 import FooterOne from "./components/FooterOne";
@@ -10,8 +10,29 @@ import { Routes, Route } from "react-router-dom";
 import ProductPage from "./components/ProductPage";
 import ProductCategory from "./components/ProductCategory";
 import Checkout from "./components/Checkout";
+import { useDispatch } from "react-redux";
+import { userLogin } from "./states/actions/userLoginActions";
 
 const User = () => {
+  const userLoginDispatch = useDispatch();
+  const authenticate = async()=>{
+    try {
+      const res = await fetch("/api/auth/authenticate",{
+        method: 'GET',
+        credentials: 'include'
+      })
+      if(res.status === 200){
+        userLoginDispatch(userLogin());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    authenticate();
+  },[])
+
   return (
     <div>
       <Navbar />
--- client/src/components/ProductPage.jsx
+++ client/src/components/ProductPage.jsx
@@ -32,11 +32,20 @@ const ProductPage = () => {
 
     let { id } = useParams();
 
+    const buyNowHandler = ()=>{
+        const item = {qty:qty, detail : {id:product._id}}
+    }
+
+    const addToCartHandler = ()=>{
+        dispatch(cartIncrement(qty));
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(`http://localhost:5000/admin/getProduct/${id}`);
 
             const data = await response.json();
+            console.log(data.product);
             setProduct(data.product);
         }
 
@@ -110,8 +119,8 @@ const ProductPage = () => {
                             <p className='flex justify-center items-center md:justify-start gap-1 text-lg cursor-pointer'><AiFillMinusCircle className='text-red-500' onClick={decQty} /> {qty} <IoMdAddCircle className='text-red-500' onClick={incQty} /></p>
                         </div>
                         <div className='flex gap-2 md:gap-4 items-center'>
-                            <Link to="/checkout"><button className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded md:text-base text-sm">Buy Now</button></Link>
-                            <button onClick = {()=>{dispatch(cartIncrement(qty))}} className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded md:text-base text-sm ">Add to Cart</button>
+                            <Link to="/checkout"><button onClick={buyNowHandler} className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded md:text-base text-sm">Buy Now</button></Link>
+                            <button onClick = {addToCartHandler} className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded md:text-base text-sm ">Add to Cart</button>
                             <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500">
                                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
